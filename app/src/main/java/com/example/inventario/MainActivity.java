e39ecdb308a6c1a41ac2d95aa74558f0c7b5741f
luissfernando <82791279+luissfernando@users.noreply.github.com>
2021-08-09 21:53:32 -0500

    tienda
    tienda

---
+++ app/src/main/java/com/example/inventario/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.inventario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sig(View v){
+        Intent i=new Intent(this,tienda.class);
+        startActivity(i);
+    }
+}
